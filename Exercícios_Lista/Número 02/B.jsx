fbfbc3489922e75247f008e807bee51ae298a8a3
Usagi-Gb <gabrieldonascimentosantosof@gmail.com>
2025-11-11 10:36:31 -0300

    adafffaf

--- Exercícios_Lista/Número 02/B.jsx
+++ Exercícios_Lista/Número 02/B.jsx
@@ -2,7 +2,6 @@ import './App.css'
 function BoasVindas({ usuario }) {
     return <h1>Bem vindo de volta, {usuario}!</h1>
 }
-
 export default function MyApp() {
     let user = prompt("Qual é o seu nome?")
     return <BoasVindas usuario={user} />
